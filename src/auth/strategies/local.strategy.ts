10b18234b63483a3ed0b5c5931c0c22d676bc7cd
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-07 22:51:49 -0300

    feat: create local strategy

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport"
+import { Strategy } from "passport-local";
+import { UsersService } from "../../users/users.service";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UsersService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password'
+    });
+  }
+
+  async validate(email: string, password: string) {
+    try {
+      const user = await this.userService.verifyUser(email, password);
+      return user;
+    }
+    catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+}
