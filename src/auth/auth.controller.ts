df28907be2b14cb79c97a355f0374d6fdeaab8de
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-11 15:23:50 -0300

    feat: implements AuthController and create interface TokenPayload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,15 +3,18 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
 import { CurrentUser } from './current-user.decorator';
 import { User } from '../users/entities/user.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
   @Post('login')
   @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response
   ) {
-
+    await this.authService.login(user, response);
   }
 }
