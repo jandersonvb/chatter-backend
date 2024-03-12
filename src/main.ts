57745c859c0ff699aca220912561f5ba2d0bc69c
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-12 10:59:35 -0300

    feat: create middleware to parser cookies

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.useLogger(app.get(Logger))
+  app.useGlobalPipes(new ValidationPipe()); // Middleware to validate requests
+  app.useLogger(app.get(Logger)) // Middleware to log requests
+  app.use(cookieParser()); // Middleware to parse cookies
 
   const configService = app.get(ConfigService)
 
