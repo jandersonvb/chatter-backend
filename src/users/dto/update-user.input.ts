9a695f42b3964592f248a70d383b5647a22a0893
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-12 15:28:01 -0300

    feat: add use guards in UsersResolver

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,5 @@
+import { InputType, PartialType } from '@nestjs/graphql';
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field()
-  _id: string;
-}
+export class UpdateUserInput extends PartialType(CreateUserInput) { }
