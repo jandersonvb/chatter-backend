2eb8a14d6e9cc846afc6271cc56bd610b5a336bc
jandersonvb <jandersonvb.dev@gmail.com>
2024-04-05 19:34:35 -0300

    feat: add databasemodule in chats module

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
+import { Chat, ChatSchema } from './entities/chat.entity';
+import { DatabaseModule } from '../common/database/database.module';
 
 @Module({
-  providers: [ChatsResolver, ChatsService, ChatsRepository],
+  imports: [
+    DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]), // Import the DatabaseModule and use the forFeature() method to define the Chat entity
+  ],
+  providers: [ChatsResolver, ChatsService, ChatsRepository]
 })
 export class ChatsModule { }
